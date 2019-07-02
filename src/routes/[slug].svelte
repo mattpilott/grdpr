245e8985bf473518f8ef86eb0ef1e5a684ae0455
Matt Pilott <matt@creativelittledots.co.uk>
2019-07-02 15:25:23 +0100

    Initial

---
+++ src/routes/[slug].svelte
@@ -0,0 +1,18 @@
+<h1>{post.title.rendered}</h1>
+
+<div>{@html post.content.rendered}</div>
+
+<script context="module">
+
+   export async function preload(page) {
+
+      const { slug } = page.params;
+      const req = await this.fetch('https://eurogroupe.org/dev/wp/wp-json/wp/v2/news?slug=' + slug).then(r => r.json());
+
+      return { post: req[0] };
+   }
+</script>
+
+<script>
+   export let post;
+</script>
